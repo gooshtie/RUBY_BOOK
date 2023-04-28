2256e457f29aaf9d21828709b69d17ed91f26b5e
Christopher Hennessy <curlyhennessymusic@gmail.com>
2023-04-28 09:51:04 -0400

    adding exercises to doc

--- the_basics/the_basics.rb
+++ the_basics/the_basics.rb
@@ -1,10 +1,27 @@
 # A literal is any notation that lets you represent a ficed value in source code.
 
-'Hello, wold!'  #string literal
-375 # integer literal
-3.141528  # float literal
-true  # boolean literal
-{ 'a' => 1, 'b' => 2 }  #hash literal
- [1,2,3]  # array literal
- :sym #symbol literal
- nil  #nil literal
+
+# 'Hello, wold!'  #string literal
+# 375 # integer literal
+# 3.141528  # float literal
+# true  # boolean literal
+# { 'a' => 1, 'b' => 2 }  #hash literal
+# [1,2,3]  # array literal
+# :sym #symbol literal
+# nil  #nil literal
+
+ first = "Chris "
+ last = "Hennessy"
+
+ puts first + last
+
+ thousands = 4936 / 1000
+ hundreds = 4936 % 1000 / 100
+ tens = 4936 % 1000 % 100 / 10
+ ones = 4936 % 1000 % 100 % 10
+
+ puts thousands
+ puts hundreds
+ puts tens
+ puts ones
+ 
