da21203c11df6ab5f37659cb023bd4eb12ab6af5
Chris Hennessy <curlyhennessymusic@gmail.com>
2023-04-29 16:02:54 -0400

    blocks and scope

--- variables/variable.rb
+++ variables/variable.rb
@@ -14,4 +14,21 @@ end
 print_full_name 'Peter', 'Henry' # prints Peter Henry
 print_full_name 'Lynn', 'Blake' # prints Lynn Blake
 print_full_name 'Kim', 'Johansson' # prints Kim Johansson
-puts name
+puts name 
+
+total = 0 
+[1,2,3].each { |number| total += number}
+puts total #6
+
+total = 0
+[1,2,3].each do |number|
+  total += number
+end
+puts total
+
+a = 5
+3.times do |n|
+  a = 3
+end
+
+puts a
