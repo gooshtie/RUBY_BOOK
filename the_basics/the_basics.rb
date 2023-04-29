591a450aaffd06097480938896efbc2fe97ff6cd
Christopher Hennessy <curlyhennessymusic@gmail.com>
2023-04-29 08:04:26 -0400

    updating the basics variables, variable.rb

--- the_basics/the_basics.rb
+++ the_basics/the_basics.rb
@@ -15,6 +15,8 @@
 
  puts first + last
 
+ "\n"
+
  thousands = 4936 / 1000
  hundreds = 4936 % 1000 / 100
  tens = 4936 % 1000 % 100 / 10
@@ -24,3 +26,40 @@
  puts "hundreds: #{hundreds}"
  puts "tens: #{tens}"
  puts "ones: #{ones}"
+
+ "\n"
+
+ # movies as hash
+  movies = { :one => 1975, :two => 2004, :three => 2013, :four => 2001, :five => 1981}
+ 
+ puts movies[:one]
+ puts movies[:two]
+ puts movies[:three] 
+ puts movies[:four]
+ puts movies[:five]
+
+"\n"
+
+ # movie dates as array
+ moviesDates = [1975, 2004, 2013, 2001, 1981]
+ 
+ puts moviesDates[0]
+ puts moviesDates[1]
+ puts moviesDates[2] 
+ puts moviesDates[3]
+ puts moviesDates[4]
+
+"\n"
+"\n"
+ puts 5*4*3*2*1
+
+
+
+ 
+ 
+
+ 
+
+
+
+
---
+++ variables/variable.rb
@@ -0,0 +1,17 @@
+# variable
+first_name = 'Joe'
+
+# method definition
+
+name = 'Somebody Else'
+
+def print_full_name(first_name, last_name)
+  name = first_name + '' + last_name
+  puts name
+end
+
+
+print_full_name 'Peter', 'Henry' # prints Peter Henry
+print_full_name 'Lynn', 'Blake' # prints Lynn Blake
+print_full_name 'Kim', 'Johansson' # prints Kim Johansson
+puts name
